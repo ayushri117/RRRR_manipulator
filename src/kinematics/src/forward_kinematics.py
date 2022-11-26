bcb47b0903029f504b4498b639c8a9f091fb8633
ayushri117 <122001010@smail.iitpkd.ac.in>
2022-11-26 22:51:55 +0530

    forward_kinematics

---
+++ src/kinematics/src/forward_kinematics.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import rospy
+import numpy
+from sensor_msgs.msg import JointState
+from math import sin,cos
+
+def tf_matrix(dh):
+    alpha=dh[0,0]
+    a=dh[0,1]
+    theta=dh[0,2]
+    d=dh[0,3]
+    tf=numpy.matrix([[cos(theta),-sin(theta),0,a],
+                     [sin(theta)*cos(alpha),cos(theta)*cos(alpha),-sin(alpha),-d*sin(alpha)],
+                     [sin(theta)*sin(alpha),cos(theta)*sin(alpha),cos(alpha),d*cos(alpha)],
+                     [0,0,0,1]])
+    return tf
+def callback(msg):
+    joints=msg.position
+    dh_table=numpy.matrix([[0,0,joints[5],120],
+                          [1.57079633,0,joints[2],0],
+                          [0,500,joints[1],0],
+                          [0,300,joints[0],0],
+                          [0,45,0,0]])
+    transformation_matrix=numpy.identity(4)
+    for val in dh_table:
+        transformation_matrix=numpy.matmul(transformation_matrix,tf_matrix(val[0]))
+    print(transformation_matrix)
+
+    
+if __name__=="__main__":
+    rospy.init_node("forward_kinematics")
+    rospy.loginfo("Forward KInematics node has been started")
+    rospy.Subscriber("/joint_states",JointState,callback=callback)
+    rospy.spin()
