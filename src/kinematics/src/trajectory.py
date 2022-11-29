a154311d56a0c7005b5fce117bf151c3a0d750dd
parapara29 <shivanshc051@gmail.com>
2022-11-29 14:26:19 +0530

    changes

--- src/kinematics/src/trajectory.py
+++ src/kinematics/src/trajectory.py
@@ -69,7 +69,7 @@ def callback(msg):
         cy=poly*coeff_y
         cz=poly*coeff_z
         cphi=poly*coeff_phi
-        l1=120
+        l1=200
         l2=500
         l3=300
         l4=45
@@ -119,8 +119,8 @@ def callback(msg):
     pub=rospy.Publisher("joint_states",JointState,queue_size=10)
     content=JointState()
     content.header=Header()
-    
-    content.name=['Rev179','Rev192','Rev193','Rev202','Rev204','Rev205']
+    content.header.stamp=rospy.Time.now()
+    content.name=['Rev179','Rev192','Rev193','Rev206','Rev207','Rev208']
     
     content.velocity=[]
     content.effort=[]
@@ -130,7 +130,7 @@ def callback(msg):
     lt2=0.0
     lt3=0.0
     lt4=0.0
-    rate=rospy.Rate(8)
+    rate=rospy.Rate(100)
     for i in theta:
         for j in i:
             if (count==1):
@@ -145,11 +145,9 @@ def callback(msg):
             elif(count==4):
                 content.position=[lt1,0,0,lt2,lt3,j]
                 lt4=j
-            content.header.stamp=rospy.Time.now()
             pub.publish(content)
-            rate.sleep()
+            
         count+=1
-    time.sleep(100)
 
 
     
